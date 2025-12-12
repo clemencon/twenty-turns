acde3464a95c39f5b4c27350609f192e5c0e3e21
Julien Clémençon <julien@clemencon.dev>
2025-12-12 14:59:23 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,31 @@
 import "./style.css";
 
 console.info("Twenty ☉ Turns");
+
+const midi = await navigator.requestMIDIAccess();
+
+midi.inputs.forEach((input) => input.onmidimessage = logMidiMessage);
+function logMidiMessage(message: MIDIMessageEvent): void {
+    if (!message.data) return;
+    console.log("🌞", message.data);
+    const [command, note, velocity] = message.data;
+    console.log(`Received: command: ${command}, note: ${note}, velocity: ${velocity}`);
+}
+
+const c4button = document.getElementById("c4");
+if (c4button === null) throw Error();
+c4button.onclick = () => playC4();
+function playC4() {
+    const output = midi.outputs.values().next().value;
+    if (!(output instanceof MIDIOutput)) throw Error();
+    output.send([0x90, 60, 100]);
+    setTimeout(() => output.send([0x80, 60, 0]), 500);
+}
+
+
+const testSlider = document.getElementById("test-slider");
+if (!(testSlider instanceof HTMLInputElement)) throw Error();
+
+testSlider.oninput = () => {
+    console.log("Slider value:", testSlider.value);
+};
